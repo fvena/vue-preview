0c6c9f52aebbe142db08d96766f50f850145fdfa
Francisco Vena <fvena32@gmail.com>
2023-05-23 21:26:09 +0200

    chore: clean project

--- src/App.vue
+++ src/App.vue
@@ -18,31 +18,4 @@ provide('clear-console', true)
   <textarea v-model="store.state.activeFile.code" />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
