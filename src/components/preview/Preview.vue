62e35a832de23087dc77d50d21fc02b9ab5669ed
Francisco Vena <fvena32@gmail.com>
2023-05-24 22:53:12 +0200

    feat: Resize iframe to fit content automatically

--- src/components/preview/Preview.vue
+++ src/components/preview/Preview.vue
@@ -8,7 +8,7 @@
 import { Preview } from '@vue/repl';
 import type { Store } from '@vue/repl'
 import { ReplStore } from '@vue/repl';
-import { onMounted, provide, watch } from 'vue'
+import { onMounted, onUnmounted, provide, watch } from 'vue'
 import { debounce } from '../../utils';
 
 export interface Props {
@@ -23,18 +23,54 @@ const props = withDefaults(defineProps<Props>(), {
 
 const { store } = props
 
-store.init()
-
 provide('store', store)
 provide('clear-console', true)
 
+/**
+ * Resize the iframe to fit the content.
+ * 
+ * @param event - The message event.
+ */
+const resize = (event: MessageEvent) => {
+  const iframe = (event.source as Window)?.frameElement as HTMLIFrameElement | null;
+  const height = iframe?.contentDocument?.querySelector('html')?.offsetHeight;
+
+  if (height) {
+    iframe!.style.height = `${height}px`;
+  }
+};
+
 onMounted(() => {
+  store.init()
   store.state.activeFile.code = props.code
+  window.addEventListener('message', resize, true)
 })
 
+onUnmounted(() => {
+  window.removeEventListener('message', resize)
+})
+
+/**
+ * Update the code in the store. 
+ * Use a debounce to prevent the store from being updated too often.
+ */
 watch(() => props.code, debounce((code: string) => {
   store.state.activeFile.code = code
 }, 250))
+
 </script>
 
-<style scoped></style>
+<style scoped>
+.preview {
+  box-sizing: content-box;
+  padding: 8px;
+}
+
+:deep(iframe) {
+  width: 100%;
+  border: none;
+  height: 100px;
+  min-height: 100px;
+  transition: height 0.3s ease-out;
+}
+</style>
